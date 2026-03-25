f1a9f9c50417db9f16d33aa69cf3850d666715fa
Kimgion <phatbo13579@gmail.com>
2026-03-25 20:32:53 +0700

    Add FlashCard form and remove TuVung navigation
    Add a new FormFlashCard UI: created FormFlashCard.cs, FormFlashCard.Designer.cs and FormFlashCard.resx with a basic layout (AntdUI.Progress, TableLayoutPanel and AntdUI.Panel) to serve as the flashcard screen skeleton. Also removed the TuVung navigation property (ThongTinTuVung) from TuVungKho to simplify the model and avoid the direct entity navigation reference.

---
+++ VocabMaster/FormFlashCard.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace VocabMaster
+{
+    partial class FormFlashCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progress1 = new AntdUI.Progress();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new AntdUI.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progress1
+            // 
+            this.progress1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progress1.Fill = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(92)))), ((int)(((byte)(169)))));
+            this.progress1.Location = new System.Drawing.Point(0, 0);
+            this.progress1.Margin = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.progress1.Name = "progress1";
+            this.progress1.Padding = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.progress1.Size = new System.Drawing.Size(1098, 23);
+            this.progress1.TabIndex = 0;
+            this.progress1.Text = "progress1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 23);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1098, 548);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.Back = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(399, 101);
+            this.panel1.Name = "panel1";
+            this.panel1.Radius = 15;
+            this.panel1.Size = new System.Drawing.Size(299, 345);
+            this.panel1.TabIndex = 0;
+            this.panel1.Text = "panel1";
+            // 
+            // FormFlashCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1098, 571);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.progress1);
+            this.Font = new System.Drawing.Font("K2D", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.Name = "FormFlashCard";
+            this.Text = "FormFlashCard";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AntdUI.Progress progress1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private AntdUI.Panel panel1;
+    }
+}
--- VocabMaster/TuVungKho.cs
+++ VocabMaster/TuVungKho.cs
@@ -8,9 +8,6 @@ namespace VocabMaster
         public int SoLanSai { get; set; }
         public DateTime NgaySaiCuoiCung { get; set; }
 
-        // Navigation property để lấy thông tin chi tiết từ vựng nếu cần
-        public TuVung ThongTinTuVung { get; set; }
-
         public TuVungKho()
         {
             NgaySaiCuoiCung = DateTime.Now;
